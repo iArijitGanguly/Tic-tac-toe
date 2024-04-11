ece1843c03886337136072b4675cd996ef940c35
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-04-11 17:49:10 +0530

    Tied logic also implemented and finalising the app

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Icon from "./Icon";
 
-const Card = ({ swapTurn, player, index }) => {
-
+const Card = ({ gameEnd, tied, swapTurn, player, index }) => {
   let icon = <Icon />;
-  if(player == 'X') {
-    icon = <Icon name={'cross'} />
-  }
-  else if(player == 'O') {
-    icon = <Icon name={'circle'} />
+  if (player == "X") {
+    icon = <Icon name={"cross"} />;
+  } else if (player == "O") {
+    icon = <Icon name={"circle"} />;
   }
   return (
     <div
-      className="border-[1px] border-solid border-black p-8 flex justify-center items-center basis-[30%] w-28 h-28 rounded-[10%] bg-yellow cursor-pointer"
-      onClick={() => swapTurn(index)}
+      className={`border-[1px] border-solid border-black p-8 flex justify-center items-center basis-[30%] w-28 h-28 rounded-[10%] bg-yellow ${
+        (gameEnd || tied) ? "cursor-default" : "cursor-pointer"
+      }`}
+      onClick={() => !gameEnd && !tied && player == "" && swapTurn(index)}
     >
       {icon}
     </div>
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,78 +1,152 @@
-import React, { useState } from 'react';
-import Card from './Card';
+import React, { useState } from "react";
+import Card from "./Card";
 
 const Grid = ({ numberOfCards }) => {
-    const [turn, setTurn] = useState(true);
-    const [board, setBoard] = useState(Array(numberOfCards).fill(''));
-    const [winner, setWinner] = useState(null);
+  const [turn, setTurn] = useState(true);
+  const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+  const [winner, setWinner] = useState(null);
+  const [tied, setTied] = useState(false);
 
-    function swapTurn(index) {
-      if(turn == true) {
-        board[index] = 'X';
-      }
-      else {
-        board[index] = 'O';
-      }
-      const win = isWinner();
-      console.log('before: ', board)
-      if(win) {
-        setWinner(win);
-        return;
-      }
-      setBoard([...board]);
-      console.log('After :', board);
-      setTurn(!turn);
+  function swapTurn(index) {
+    if (turn == true) {
+      board[index] = "X";
+    } else {
+      board[index] = "O";
+    }
+    const win = isWinner();
+    if (win) {
+      setWinner(win);
+    }
+    checkTied();
+    setBoard([...board]);
+    setTurn(!turn);
+  }
+
+  function isWinner() {
+    if (
+      (board[0] != "" || board[1] != "" || board[2] != "") &&
+      board[0] == board[1] &&
+      board[1] == board[2]
+    ) {
+      return board[0];
+    }
+    if (
+      (board[3] != "" || board[4] != "" || board[5] != "") &&
+      board[3] == board[4] &&
+      board[4] == board[5]
+    ) {
+      return board[3];
+    }
+    if (
+      (board[6] != "" || board[7] != "" || board[8] != "") &&
+      board[6] == board[7] &&
+      board[7] == board[8]
+    ) {
+      return board[6];
     }
 
-    function isWinner() {
-      if((board[0] != '' || board[1] != '' || board[2] != '') && (board[0] == board[1] && board[1] == board[2])) {
-        return board[0];
-      }
-      if((board[3] != '' || board[4] != '' || board[5] != '') && (board[3] == board[4] && board[4] == board[5])) {
-        return board[3];
-      }
-      if((board[6] != '' || board[7] != '' || board[8] != '') && (board[6] == board[7] && board[7] == board[8])) {
-        return board[6];
-      }
-
-
-      if((board[0] != '' || board[3] != '' || board[6] != '') && (board[0] == board[3] && board[3] == board[6])) {
-        return board[0];
-      }
-      if((board[1] != '' || board[4] != '' || board[7] != '') && (board[1] == board[4] && board[4] == board[7])) {
-        return board[1];
-      }
-      if((board[2] != '' || board[5] != '' || board[8] != '') && (board[2] == board[5] && board[5] == board[8])) {
-        return board[2];
-      }
-
-
-      if((board[0] != '' || board[4] != '' || board[8] != '') && (board[0] == board[4] && board[4] == board[8])) {
-        return board[0];
-      }
-      if((board[2] != '' || board[4] != '' || board[6] != '') && (board[2] == board[4] && board[4] == board[6])) {
-        return board[2];
-      }
-
-      return '';
+    if (
+      (board[0] != "" || board[3] != "" || board[6] != "") &&
+      board[0] == board[3] &&
+      board[3] == board[6]
+    ) {
+      return board[0];
+    }
+    if (
+      (board[1] != "" || board[4] != "" || board[7] != "") &&
+      board[1] == board[4] &&
+      board[4] == board[7]
+    ) {
+      return board[1];
+    }
+    if (
+      (board[2] != "" || board[5] != "" || board[8] != "") &&
+      board[2] == board[5] &&
+      board[5] == board[8]
+    ) {
+      return board[2];
     }
 
-    function resetGame() {
-      setBoard(Array(numberOfCards).fill(''));
-      setTurn(true);
-      setWinner(null);
+    if (
+      (board[0] != "" || board[4] != "" || board[8] != "") &&
+      board[0] == board[4] &&
+      board[4] == board[8]
+    ) {
+      return board[0];
     }
+    if (
+      (board[2] != "" || board[4] != "" || board[6] != "") &&
+      board[2] == board[4] &&
+      board[4] == board[6]
+    ) {
+      return board[2];
+    }
+
+    return "";
+  }
+
+  function checkTied() {
+    let fillCount = 0;
+    board.forEach((box) => {
+      if (box != "") {
+        fillCount++;
+      }
+    });
+
+    if (fillCount == 9) {
+      setTied(true);
+    }
+  }
+
+  function resetGame() {
+    setBoard(Array(numberOfCards).fill(""));
+    setTurn(true);
+    setWinner(null);
+    setTied(false);
+  }
   return (
-    <div>
-        {winner && <h1 className='text-yellow'>Winner is : {winner}</h1>}
-        <h1 className='text-yellow'>Current Turn: {(turn) ? 'X' : 'O'} </h1>
-        <div className='flex w-[31.25rem] h-[31.25rem] justify-between items-center flex-wrap'>
-            {board.map((player, index) => {
-                return <Card key={index} player={player} swapTurn={swapTurn} index={index}/>
-            })}
-        </div>
+    <div className="max-w-md">
+      {tied && (
+        <>
+          <h1 className="text-yellow">Game is Tied now !!!!</h1>
+          <button
+            className="w-full bg-green-600 text-white p-2 rounded-lg"
+            onClick={resetGame}
+          >
+            Reset Game
+          </button>
+        </>
+      )}
+      {winner && (
+        <>
+          <h1 className="text-yellow">Winner is : {winner}</h1>
+          <button
+            className="w-full bg-green-600 text-white p-2 rounded-lg"
+            onClick={resetGame}
+          >
+            Reset Game
+          </button>
+        </>
+      )}
+      {!winner && !tied && (
+        <h1 className="text-yellow">Current Turn: {turn ? "X" : "O"} </h1>
+      )}
+      <div className="flex w-[31.25rem] h-[31.25rem] justify-between items-center flex-wrap">
+        {board.map((player, index) => {
+          return (
+            <Card
+              key={index}
+              gameEnd={winner ? true : false}
+              tied={tied}
+              player={player}
+              swapTurn={swapTurn}
+              index={index}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Grid;
